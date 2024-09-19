b6d410824dbf498acbce85fe532283768bee73bf
Sachin Gabhud <sachin.gabhud@netenrich.com>
2024-09-19 08:48:43 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: "Which is not a keyword in JavaScript?",
+    answer: [
+      { option: "let", correct: false },
+      { option: "var", correct: false },
+      { option: "const", correct: false },
+      { option: "temp", correct: false },
+    ],
+  },
+  {
+    question: "Dummy Question?",
+    answer: [
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+    ],
+  },
+  {
+    question: "Dummy Question?",
+    answer: [
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+    ],
+  },
+  {
+    question: "Dummy Question?",
+    answer: [
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+      { option: "let", correct: false },
+    ],
+  },
+];
